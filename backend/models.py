95cb1e1e9fece8576826469a495369dbec06a74e
Bazbamboozka <monishb1511@gmail.com>
2026-03-14 03:35:29 +0530

    Reset database schema

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+
 db = SQLAlchemy()
 
 
@@ -20,16 +21,14 @@ class Request(db.Model):
     __tablename__ = "request"
 
     id = db.Column(db.Integer, primary_key=True)
-
     location_text = db.Column(db.String(200))
     issue_description = db.Column(db.String(500))
     vehicle = db.Column(db.String(50))
-
-
     status = db.Column(db.String(50), default="pending")
 
     customer_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     provider_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
+
     price = db.Column(db.Float, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     rating = db.Column(db.Integer, nullable=True)
